d291ec0b08dc92951d6ce5fefef3a3a39c36bce2
Ethan-Witter-Student <112420827+EthanWitterStudent@users.noreply.github.com>
2025-06-05 23:14:18 -0400

    Added multiple coin tosses

--- CoinToss.java
+++ CoinToss.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,41 +8,83 @@ public class CoinToss {
 	
 	public static void main(String[] args) //heres where we be tossing them coins!
 	{
+		//Inital set up of random and scanner (for user input)
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
-		boolean coin = random.nextBoolean(); //True is Heads, False is Tails (gurantees only 2 outcomes)
 		
-		//existing for testing purposes
-		//System.out.println(coin);
+		System.out.println("Welcome to the coin toss game!");
+		System.out.println("In this game, coins are tossed, and you guess Heads or Tails. Now:");
 		
-		boolean acceptableGuess = false; //for exiting exception loop
-		while(acceptableGuess == false)
+		int numOfTosses = 0;
+		boolean validNumber = false; //for exiting exception loop
+		while (validNumber == false)
 		{
-			System.out.println("Heads or Tails? (enter 'true' or 'false' to guess Heads or Tails)");
+			System.out.println("How many coins do you want to toss?");
 			try
-			{	boolean playerGuess;
-				playerGuess = scanner.nextBoolean();
-				
-				if(playerGuess == coin)
-				{
-					System.out.println("CORRECT! Nice job!");
-				}
-				else
-				{
-					System.out.println("Unfortunatly that is incorrect. Good try though");
-				}
-				
-				acceptableGuess = true; //sucessful guess, exit loop
-				scanner.close();
+			{
+				numOfTosses = scanner.nextInt();
+				System.out.println("Alright! " + numOfTosses + " coins will be tossed!");
+				validNumber = true;
 			}
 			catch (Exception e)
 			{
-				System.out.println("Thats not a valid guess. Please try again");
+				System.out.println("Thats not a valid number. Please try again");
 				scanner.nextLine();
 			}
 		}
 		
+		scanner.nextLine();
+			
+		//fleshes out a list of coin tosses. The list of coin tosses is equal to what the user specified
+		List<Boolean> coinTosses = new ArrayList<>();
+		for(int i = 0; i < numOfTosses; i++)
+		{
+			boolean value = random.nextBoolean();
+			coinTosses.add(value);
+		}
+
 		
+		//List is used to track how many guess the player got correct
+		List<Boolean> didPlayerGuessCorrect = new ArrayList<>();
+		System.out.println("Now let the guessing begin!");
+		for(int i = 0; i < numOfTosses; i++)
+		{
+			int guessTextNum = i+1; //exists purely for the player's message, telling them what guess he's on.
+			System.out.println("Guess number " + guessTextNum);
+			boolean acceptableGuess = false; //for exiting exception loop
+			while(acceptableGuess == false)
+			{
+				System.out.println("Heads or Tails? (enter 'true' or 'false' to guess Heads or Tails)");
+				try
+				{	boolean playerGuess;
+					playerGuess = scanner.nextBoolean();
+					
+					if(playerGuess == coinTosses.get(i))
+					{
+						System.out.println("CORRECT! Nice job!");
+						didPlayerGuessCorrect.add(true);
+					}
+					else
+					{
+						System.out.println("Unfortunatly that is incorrect. Good try though!");
+						didPlayerGuessCorrect.add(false);
+						
+					}
+					
+					scanner.nextLine(); //needed since many guesses will be made
+					acceptableGuess = true; //sucessful guess, exit loop
+				}
+				catch (Exception e)
+				{
+					System.out.println("Thats not a valid guess. Please try again");
+					scanner.nextLine();
+				}
+			}
+			
+		}
+		System.out.println("Game over! Hope you enjoyed!");
+		scanner.close();
 		
+
 	}
 }
