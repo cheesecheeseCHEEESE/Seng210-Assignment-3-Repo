be8cb29da546787ac1ba74c441fde78212c57d37
Ethan-Witter-Student <112420827+EthanWitterStudent@users.noreply.github.com>
2025-06-11 14:43:30 -0400

    started work on Streak Gamemode

--- CoinToss.java
+++ CoinToss.java
@@ -73,10 +73,6 @@ public class CoinToss {
 						acceptableAnswer = true;
 						//scanner.nextLine();
 				}
-				/*else if(answer == false)
-				{
-					Unused as of now, kept here incase that needs to change
-				}*/
 				else
 				{
 					System.out.println("You must like Vanilla Icecream. Anyway:");
@@ -94,120 +90,190 @@ public class CoinToss {
 		
 		
 		int numOfTosses = 0;
-		boolean validNumber = false; //for exiting exception loop
-		while (validNumber == false)
+		int selectedGamemode = 0; //determined outside because this is used to skip over certain chunks of code
+		boolean validGamemode = false;
+		while(!validGamemode)
 		{
-			System.out.println("How many coins do you want to toss?");
-			try
+			try 
 			{
-				numOfTosses = scanner.nextInt();
-				System.out.println("Alright! " + numOfTosses + " coins will be tossed!");
-				validNumber = true;
+				System.out.println("Enter what gamemode you would you like to play");
+				System.out.println("1 for Normal");
+				System.out.println("2 for Streak (game ends when you guess wrong)");
+				selectedGamemode = scanner.nextInt();
+				if(selectedGamemode == 1)
+				{
+					validGamemode = true;
+					System.out.println("Normal Mode selected");
+				}
+				else if (selectedGamemode == 2)
+				{
+					validGamemode = true;
+					System.out.println("Streak Mode selected");
+				}
 			}
 			catch (Exception e)
 			{
-				System.out.println("Thats not a valid number. Please try again");
-				scanner.nextLine();
+				System.out.println("Invalid Gamemode");
 			}
 		}
 		
-		scanner.nextLine();
-			
-		//fleshes out a list of coin tosses. The list of coin tosses is equal to what the user specified
-		List<Boolean> coinTosses = new ArrayList<>();
-		for(int i = 0; i < numOfTosses; i++)
+		if(selectedGamemode == 1) //All of the original gameplay code is here, since it was normal mode
 		{
-			boolean value = random.nextBoolean();
-			coinTosses.add(value);
-		}
-		
-
-		int correctGuessStreak = 0; //the current streak of correct guesses
-		int highestStreak = 0; //the highest streak of guesses the player has
-		
-		//List is used to track how many guess the player got correct
-		List<Boolean> didPlayerGuessCorrect = new ArrayList<>();
-		System.out.println("Now let the guessing begin!");
-		for(int i = 0; i < numOfTosses; i++)
-		{
-			int guessTextNum = i+1; //exists purely for the player's message, telling them what guess he's on.
-			System.out.println("Guess number " + guessTextNum);
-			boolean acceptableGuess = false; //for exiting exception loop
-			while(acceptableGuess == false)
+			boolean validNumber = false; //for exiting exception loop
+			while (validNumber == false)
 			{
-				System.out.println(coinHeadFace + " or " +coinTailFace+ "? (enter 'true' or 'false' to guess)");
+				System.out.println("How many coins do you want to toss?");
 				try
-				{	boolean playerGuess;
-					playerGuess = scanner.nextBoolean();
-					
-					if(playerGuess == coinTosses.get(i))
-					{
-						System.out.println("CORRECT! Nice job!");
-						correctGuessStreak++;
-						didPlayerGuessCorrect.add(true);
-					}
-					else
-					{
-						System.out.println("Unfortunatly that is incorrect. Good try though!");
-						//set the highest streak to the current streak if current streak is higher
-						if(correctGuessStreak > highestStreak)
-						{
-							highestStreak = correctGuessStreak;
-						}
-						correctGuessStreak = 0;
-						didPlayerGuessCorrect.add(false);
-						
-					}
-					
-					scanner.nextLine(); //needed since many guesses will be made
-					acceptableGuess = true; //sucessful guess, exit loop
+				{
+					numOfTosses = scanner.nextInt();
+					System.out.println("Alright! " + numOfTosses + " coins will be tossed!");
+					validNumber = true;
 				}
 				catch (Exception e)
 				{
-					System.out.println("Thats not a valid guess. Please try again");
+					System.out.println("Thats not a valid number. Please try again");
 					scanner.nextLine();
 				}
 			}
-		}
-		
-		//run one last time after the loop in case the player got ALL of them correct
-		if(correctGuessStreak > highestStreak)
-		{
-			highestStreak = correctGuessStreak;
-		}
-		
-		int correctGuesses = 0;
-		for(boolean guess: didPlayerGuessCorrect)
-		{
-			if(guess == true)
+			
+			scanner.nextLine();
+				
+			//fleshes out a list of coin tosses. The list of coin tosses is equal to what the user specified
+			List<Boolean> coinTosses = new ArrayList<>();
+			for(int i = 0; i < numOfTosses; i++)
 			{
-				correctGuesses++;
+				boolean value = random.nextBoolean();
+				coinTosses.add(value);
 			}
-		}
-		float successRatio = ((float) correctGuesses / numOfTosses) * 100;
-		
-		int numOfHeads = 0;
-		int numOfTails = 0;
-		for(boolean coin : coinTosses)
-		{
-			if(coin == true)
-			{
-				numOfHeads++;
-			}
-			else //assumes the alternative is tails
-			{
-				numOfTails++;
-			}
-		}
-		
-		
-		System.out.println("Game over!");
-		System.out.println("There were "+numOfHeads+" "+coinHeadFace+" and "+numOfTails+" "+coinTailFace);
-		System.out.println("You guessed correctly "+successRatio+"% of the time!");
-		System.out.println("Your highest correct guess streak was "+highestStreak+" in a row!");
-		System.out.println("Hope you enjoyed!");
-		scanner.close();
-		
+			
 
+			int correctGuessStreak = 0; //the current streak of correct guesses
+			int highestStreak = 0; //the highest streak of guesses the player has
+			
+			//List is used to track how many guess the player got correct
+			List<Boolean> didPlayerGuessCorrect = new ArrayList<>();
+			System.out.println("Now let the guessing begin!");
+			for(int i = 0; i < numOfTosses; i++)
+			{
+				int guessTextNum = i+1; //exists purely for the player's message, telling them what guess he's on.
+				System.out.println("Guess number " + guessTextNum);
+				boolean acceptableGuess = false; //for exiting exception loop
+				while(acceptableGuess == false)
+				{
+					System.out.println(coinHeadFace + " or " +coinTailFace+ "? (enter 'true' or 'false' to guess)");
+					try
+					{	boolean playerGuess;
+						playerGuess = scanner.nextBoolean();
+						
+						if(playerGuess == coinTosses.get(i))
+						{
+							System.out.println("CORRECT! Nice job!");
+							correctGuessStreak++;
+							didPlayerGuessCorrect.add(true);
+						}
+						else
+						{
+							System.out.println("Unfortunatly that is incorrect. Good try though!");
+							//set the highest streak to the current streak if current streak is higher
+							if(correctGuessStreak > highestStreak)
+							{
+								highestStreak = correctGuessStreak;
+							}
+							correctGuessStreak = 0;
+							didPlayerGuessCorrect.add(false);
+							
+						}
+						
+						scanner.nextLine(); //needed since many guesses will be made
+						acceptableGuess = true; //sucessful guess, exit loop
+					}
+					catch (Exception e)
+					{
+						System.out.println("Thats not a valid guess. Please try again");
+						scanner.nextLine();
+					}
+				}
+			}
+			
+			//run one last time after the loop in case the player got ALL of them correct
+			if(correctGuessStreak > highestStreak)
+			{
+				highestStreak = correctGuessStreak;
+			}
+			
+			int correctGuesses = 0;
+			for(boolean guess: didPlayerGuessCorrect)
+			{
+				if(guess == true)
+				{
+					correctGuesses++;
+				}
+			}
+			float successRatio = ((float) correctGuesses / numOfTosses) * 100;
+			
+			int numOfHeads = 0;
+			int numOfTails = 0;
+			for(boolean coin : coinTosses)
+			{
+				if(coin == true)
+				{
+					numOfHeads++;
+				}
+				else //assumes the alternative is tails
+				{
+					numOfTails++;
+				}
+			}
+			
+			
+			System.out.println("Game over!");
+			System.out.println("There were "+numOfHeads+" "+coinHeadFace+" and "+numOfTails+" "+coinTailFace);
+			System.out.println("You guessed correctly "+successRatio+"% of the time!");
+			System.out.println("Your highest correct guess streak was "+highestStreak+" in a row!");
+			System.out.println("Hope you enjoyed!");
+			scanner.close();
+		}
+		else if(selectedGamemode == 2)
+		{
+			boolean brokeStreak = false;
+			int streak = 0;
+			System.out.println("Now let the guessing begin!");
+			while(!brokeStreak)
+			{
+				boolean acceptableGuess = false; //for exiting exception loop
+				boolean headsOrTails = random.nextBoolean();
+				while(acceptableGuess == false)
+				{
+					System.out.println(coinHeadFace + " or " +coinTailFace+ "? (enter 'true' or 'false' to guess)");
+					try
+					{	boolean playerGuess;
+						playerGuess = scanner.nextBoolean();
+						
+						if(playerGuess == headsOrTails)
+						{
+							System.out.println("CORRECT! Nice job!");
+							streak++;
+							acceptableGuess = true;
+						}
+						else
+						{
+							System.out.println("oh...unfortunate");
+							brokeStreak = true;
+							acceptableGuess = true;
+						}
+						
+						scanner.nextLine(); //needed since many guesses will be made
+						acceptableGuess = true; //sucessful guess, exit loop
+					}
+					catch (Exception e)
+					{
+						System.out.println("Thats not a valid guess. Please try again");
+						scanner.nextLine();
+					}
+					}
+				
+			
+			}
+		}
 	}
 }
